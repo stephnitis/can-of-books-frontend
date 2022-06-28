865e12158feb91b5d39f4b0cad28cf375754b1e6
Stephanie Hill <stephni.hill@gmail.com>
2022-06-27 19:17:42 -0700

    struggling with carousel

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import axios from 'axios';
+
+let SERVER = process.env.REACT_APP_SERVER;
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -10,10 +14,40 @@ class BestBooks extends React.Component {
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
+  getBooks = async () => {
+    console.log('books fired');
+    try {
+      let results = await axios.get(`${SERVER}/books`);
+      console.log(results.data);
+      this.setState({
+        books: results.data
+      })
+    } catch(error){
+      console.log('error:', error.response.data)
+    }
+  }
+
+  componentDidMount(){
+    this.getBooks();
+  }
+
   render() {
-
+    
+      let bookCarousel = this.state.books.map((book, index) => (
+      <Carousel.Item key={index}>
+        <img
+        className="photos"
+        src="jaredd-craig-HH4WBGNyltc-unsplash.jpg"
+        alt="slide one"
+        />
+        <Carousel.Caption>{book.title}</Carousel.Caption>
+        <Carousel.Caption>{book.description}</Carousel.Caption>
+      </Carousel.Item>
+      
+      
+    ))
     /* TODO: render all the books in a Carousel */
-
+console.log(bookCarousel);
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
